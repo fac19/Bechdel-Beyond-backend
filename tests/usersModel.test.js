ee8de53c9db3c822fbd17fd7a56be532dd1328cf
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-05-21 23:37:40 +0100

    update readme

--- tests/usersModel.test.js
+++ tests/usersModel.test.js
@@ -1,51 +1,51 @@
 const test = require('tape');
 const build = require('../database/build');
 
-const { createUser, getUser } = require('../model/usersModel');
+const { createUser, getUser } = require('../model/userModel');
 
 test('DB tests are running!', (t) => {
-  const x = 5;
-  t.equal(x, 5, 'this is working');
-  t.end();
+	const x = 5;
+	t.equal(x, 5, 'this is working');
+	t.end();
 });
 
 test('Can create new user', (t) => {
-  build().then(() => {
-    const user = {
-      username: 'Bob123',
-      email: 'bob@hello.com',
-      password: '54321',
-    };
-    createUser(user)
-      .then((userId) => {
-        t.equal(userId, 3, 'User Id is equal to 3');
-        t.end();
-      })
-      .catch((err) => {
-        t.error(err);
-        t.end();
-      });
-  });
+	build().then(() => {
+		const user = {
+			username: 'Bob123',
+			email: 'bob@hello.com',
+			password: '54321',
+		};
+		createUser(user)
+			.then((userId) => {
+				t.equal(userId, 3, 'User Id is equal to 3');
+				t.end();
+			})
+			.catch((err) => {
+				t.error(err);
+				t.end();
+			});
+	});
 });
 
 test('Can  not create dublicate users', (t) => {
-  build().then(() => {
-    const user = {
-      username: 'Gio',
-      email: 'gio@gio.com',
-      password: '12367',
-    };
-    createUser(user)
-      .then(() => {})
-      .catch((err) => {
-        t.throws(
-          err.message,
-          'duplicate key value violates unique constraint "users_username_key"',
-          'User was not created',
-        );
-        t.end();
-      });
-  });
+	build().then(() => {
+		const user = {
+			username: 'Gio',
+			email: 'gio@gio.com',
+			password: '12367',
+		};
+		createUser(user)
+			.then(() => {})
+			.catch((err) => {
+				t.throws(
+					err.message,
+					'duplicate key value violates unique constraint "users_username_key"',
+					'User was not created',
+				);
+				t.end();
+			});
+	});
 });
 
 // test('Returns error if no user found', (t) => {
@@ -58,15 +58,15 @@ test('Can  not create dublicate users', (t) => {
 // });
 
 test('Returns user with a given email address', (t) => {
-  build().then(() => {
-    getUser('gio@gio.com')
-      .then((res) => {
-        t.equal(res.username, 'Gio', 'Correct name returned');
-        t.end();
-      })
-      .catch((err) => {
-        t.error(err);
-        t.end();
-      });
-  });
+	build().then(() => {
+		getUser('gio@gio.com')
+			.then((res) => {
+				t.equal(res.username, 'Gio', 'Correct name returned');
+				t.end();
+			})
+			.catch((err) => {
+				t.error(err);
+				t.end();
+			});
+	});
 });

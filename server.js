45361ab39248f0272a97b955b664c1af684b7874
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-05-21 13:26:23 +0100

    enable cors

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-const express = require("express");
-const handleError = require("./middleware/error");
-const setupMovies = require('./database/fetch')
+const express = require('express');
+const cors = require('cors');
+const handleError = require('./middleware/error');
+const setupMovies = require('./database/fetch');
 
 const PORT = process.env.PORT || 3000;
 const { signup, logIn } = require('./handlers/usersHandlers');
 const server = express();
+server.use(cors());
 server.use(express.json());
 //server routes
 server.post('/user', signup);
 server.post('/login', logIn);
 
-server.get('/', (req, res, next) => res.send("hi"));
+server.get('/', (req, res, next) => res.send('hi'));
 
 // server.get('/', setupMovies)
-
 
 server.use(handleError);
 
